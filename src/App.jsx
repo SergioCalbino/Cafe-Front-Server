ef7e33ad39752a8e26f6efe0a647d65a07eb23e5
Your Name <you@example.com>
2023-04-01 20:55:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+
+    <h1 className="font-bold text-2xl " >Hola Vite</h1>
+    <button className="btn bg-teal-400 rounded-none " >Button</button>
+    </>
+  )
+}
+
+export default App
